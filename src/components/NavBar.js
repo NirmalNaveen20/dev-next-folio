d34289c334b429a81efd934a5e0f0023ef067460
NirmalNaveen20 <nirmalnaveen95@gmail.com>
2023-07-08 17:57:10 +0530

    add icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import Logo from "./Logo";
+import LinkedinIcon, { GithubIcon } from "./Icons";
+import { motion } from 'framer-motion'
+
 
 const CustomLink = ({ href, title, className = "" }) => {
-    return (
-        <Link href={href} className={`${className}`}>
-            {title}
-        </Link>
-    )
-}
+  const router = useRouter();
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 const NavBar = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
@@ -19,19 +32,18 @@ const NavBar = () => {
         <CustomLink href="articles" title="Blog" className="mr-4" />
       </nav>
 
-      <nav>
-        <Link href="/" target={"_blank"}>T</Link>
-        <Link href="/" target={"_blank"}>T</Link>
-        <Link href="/" target={"_blank"}>T</Link>
-        <Link href="/" target={"_blank"}>T</Link>
-        <Link href="/" target={"_blank"}>T</Link>
-        <Link href="/" target={"_blank"}>T</Link>
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a href="https://github.com" target={"_blank"} whileHover={{y:-2}} whileTap={{scale:0.9}} className="w-6 mx-3">
+          <LinkedinIcon />
+        </motion.a>
+        <motion.a href="https://github.com" target={"_blank"} whileHover={{y:-2}} whileTap={{scale:0.9}} className="w-6 mx-3">
+          <GithubIcon />
+        </motion.a>
       </nav>
-      
+
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
       </div>
-      
     </header>
   );
 };
